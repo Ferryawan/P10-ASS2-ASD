a229fc7dda0f1a302fa8cb0ddc3e7bde716357ea
Ferryawan <65054970+Ferryawan@users.noreply.github.com>
2022-11-27 00:49:11 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,59 @@
+package Assessment.java.antrianpasien;
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Queue queue = new Queue();
+
+        Scanner input = new Scanner(System.in);
+
+        String loop = "y";
+        do {
+            System.out.println("Selamat datang di aplikasi antrian");
+            System.out.println("Silakan pilih menu");
+            System.out.println("1. Daftar");
+            System.out.println("2. Panggil");
+            System.out.println("3. Keluar");
+            System.out.print("Pilihan Anda : ");
+            int pilih = input.nextInt();
+
+            System.out.println("");
+
+            switch (pilih) {
+                case 1 -> {
+                    System.out.print("Masukan nama : ");
+                    String daftar = input.next();
+                    queue.enqueue(daftar);
+                    System.out.println(daftar + ", antrian anda bernomor " + (queue.peek() + 1));
+                    System.out.println("");
+                }
+                case 2 -> {
+                    if (queue.size() > 1) {
+                        System.out.println("No antrian " + (queue.peek() + 1) + ", silahkan masuk");
+                        queue.dequeue();
+                        System.out.println("");
+                    } else {
+                        System.out.println("Tidak ada antrian");
+                        System.out.println("");
+                    }
+                }
+                case 3 -> {
+                    if (queue.size() > 0) {
+                        System.out.println("Masih ada antrian dalam system");
+                        System.out.println("");
+                    } else {
+                        loop = "t";
+                    }
+                }
+                default -> {
+                    System.out.println("Pilihan tidak tersedia");
+                    System.out.println("");
+                }
+            }
+        } while (!loop.equals("t"));
+
+        System.out.println("Terima kasih telah menggunakan program ini");
+    }
+}
---
+++ Queue.java
@@ -0,0 +1,38 @@
+package Assessment.java.antrianpasien;
+
+public class Queue {
+
+    private LinkedList<Integer> list = new LinkedList<>();
+
+    public int size() {
+        return list.size();
+    }
+
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    public void enqueue(String daftar) {
+        list.addLast(daftar);
+    }
+
+    public int dequeue() {
+        return list.removeFirst();
+    }
+
+    public int first() {
+        return list.first();
+    }
+
+    public int last() {
+        return list.last();
+    }
+
+    public void print() {
+        list.print();
+    }
+
+    public int peek() {
+        return 0;
+    }
+}
